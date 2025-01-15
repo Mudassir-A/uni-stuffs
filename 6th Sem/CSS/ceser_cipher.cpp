5c253b24ca37c9fd66e1e335758fc4f8e44ead6b
Mohd Mudassir Ansari <91003890+Mudassir-A@users.noreply.github.com>
2025-01-15 05:30:17 +0000

    substitution cipher

---
+++ 6th Sem/CSS/ceser_cipher.cpp
@@ -0,0 +1,62 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Caesar_Cipher
+{
+public:
+    string encrypt(string text, int s)
+    {
+        int n = text.size();
+        string ans = "";
+        for (int i = 0; i < n; i++)
+        {
+            // capitals
+            if (isupper(text[i]))
+            {
+                ans += char(int(text[i] + s - 65) % 26 + 65);
+            }
+            // small
+            else
+            {
+                ans += char(int(text[i] + s - 97) % 26 + 97);
+            }
+        }
+        return ans;
+    }
+
+    string decrypt(string text, int s)
+    {
+        int n = text.size();
+        string ans = "";
+        for (int i = 0; i < n; i++)
+        {
+            // capitals
+            if (isupper(text[i]))
+            {
+                ans += char(int(text[i] - s - 65) % 26 + 65);
+            }
+            // small
+            else
+            {
+                ans += char(int(text[i] - s - 97) % 26 + 97);
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    // Write C++ code here
+    Caesar_Cipher cc;
+    string text = "Hello";
+    int shift = 5;
+    string res = cc.encrypt(text, shift);
+    cout << "Text: " << text << endl;
+    cout << "Shift: " << shift << endl;
+    cout << "Cipher: " << res << endl;
+    cout << "Decrypted: " << cc.decrypt(res, shift);
+    return 0;
+}
